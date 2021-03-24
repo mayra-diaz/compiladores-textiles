6719a3ebc517d66bbc87617af3b7f0aec84bb47b
lordjuacs <juacsr@gmail.com>
2021-03-24 18:14:45 -0500

    handler

--- CFGHandler/CFGHandler.cpp
+++ CFGHandler/CFGHandler.cpp
@@ -3,7 +3,7 @@
 #include <utility>
 
 CFGHandler::CFGHandler(std::string grammari, std::string terminalsi, std::string non_terminalsi,
-                       std::string starti): initial(std::move(starti)){
+                       std::string starti) : initial(std::move(starti)) {
     read_grammar(std::move(grammari));
     read_elements(std::move(terminalsi));
     read_elements(std::move(non_terminalsi), false);
@@ -12,15 +12,15 @@ CFGHandler::CFGHandler(std::string grammari, std::string terminalsi, std::string
 
 void CFGHandler::read_grammar(std::string grammari) {
     int i = 0;
-    while (i < grammari.size()){
+    while (i < grammari.size()) {
         std::string current_rule;
         while (grammari[i] != ':')
             current_rule.push_back(grammari[i++]);
         grammar.insert({current_rule, Rule_t()});
         i += 2;
         Production_t production;
-        while (grammari[i] != '$' && i < grammari.size())  {
-            if (grammari[i]=='|') {
+        while (grammari[i] != '$' && i < grammari.size()) {
+            if (grammari[i] == '|') {
                 i += 2;
                 grammar[current_rule].push_back(production);
                 production.clear();
@@ -28,24 +28,24 @@ void CFGHandler::read_grammar(std::string grammari) {
             std::string current;
             while (grammari[i] != ' ' && i < grammari.size())
                 current.push_back(grammari[i++]);
-           production.push_back(current) ;
+            production.push_back(current);
             while (grammari[i] == ' ' && i < grammari.size())
                 ++i;
         }
         grammar[current_rule].push_back(production);
-        i+=2;
+        i += 2;
     }
 }
 
-void CFGHandler::read_elements(std::string elementsi, bool is_ter){
-    int i=0;
+void CFGHandler::read_elements(std::string elementsi, bool is_ter) {
+    int i = 0;
     SetS_t *elements;
     if (is_ter)
         elements = &terminals;
     else
         elements = &non_terminals;
 
-    while (i < elementsi.size()){
+    while (i < elementsi.size()) {
         if (elementsi[i] == ' ')
             ++i;
         else {
@@ -57,27 +57,26 @@ void CFGHandler::read_elements(std::string elementsi, bool is_ter){
     }
 }
 
-void CFGHandler::generate_firsts_follows(){
-    for (const auto& nterminal: non_terminals)
+void CFGHandler::generate_firsts_follows() {
+    for (const auto &nterminal: non_terminals)
         Firsts.insert({nterminal, SetS_t()});
-    for (const auto& nterminal: non_terminals)
+    for (const auto &nterminal: non_terminals)
         Follows.insert({nterminal, SetS_t()});
     Follows[initial].insert("$");
 
     int changes = 1;
     while (changes != 0) {
         changes = 0;
-        for (const auto& rule: grammar){
-            for (auto production: rule.second){
+        for (const auto &rule: grammar) {
+            for (auto production: rule.second) {
                 if (terminals.find(production[0]) != terminals.end()) {
                     if (Firsts[rule.first].find(production[0]) == Firsts[rule.first].end()) {
                         Firsts[rule.first].insert(production[0]);
                         ++changes;
                     }
-                }
-                else {
-                    for (const auto &first: Firsts[production[0]]){
-                        if (Firsts[rule.first].find(first) == Firsts[rule.first].end()){
+                } else {
+                    for (const auto &first: Firsts[production[0]]) {
+                        if (Firsts[rule.first].find(first) == Firsts[rule.first].end()) {
                             Firsts[rule.first].insert(first);
                             ++changes;
                         }
@@ -90,9 +89,9 @@ void CFGHandler::generate_firsts_follows(){
     changes = 1;
     while (changes != 0) {
         changes = 0;
-        for (const auto& rule: grammar){
-            for (auto production: rule.second){
-                for (int i = 0; i < production.size()-1; ++i) {
+        for (const auto &rule: grammar) {
+            for (auto production: rule.second) {
+                for (int i = 0; i < production.size() - 1; ++i) {
                     if (non_terminals.find(production[i]) != non_terminals.end()) {
                         auto next = production[i + 1];
                         auto current = production[i];
@@ -111,7 +110,7 @@ void CFGHandler::generate_firsts_follows(){
                         }
                     }
                 }
-                if (non_terminals.find(production[production.size()-1]) != non_terminals.end()) {
+                if (non_terminals.find(production[production.size() - 1]) != non_terminals.end()) {
                     auto last = production[production.size() - 1];
                     if (non_terminals.find(last) != non_terminals.end()) {
                         for (const auto &t: Follows[rule.first]) {
@@ -129,7 +128,7 @@ void CFGHandler::generate_firsts_follows(){
 
 void CFGHandler::print() {
     std::cout << "GRAMMAR\n";
-    for (auto x: grammar){
+    for (auto x: grammar) {
         std::cout << x.first << " -->  ";
         for (auto v: x.second) {
             for (auto v1: v)
@@ -145,14 +144,14 @@ void CFGHandler::print() {
     for (auto n: non_terminals)
         std::cout << n << ' ';
     std::cout << "\n\nFIRSTS\n";
-    for (auto x: Firsts){
+    for (auto x: Firsts) {
         std::cout << x.first << ":  ";
         for (auto v: x.second)
             std::cout << v << ' ';
         std::cout << '\n';
     }
     std::cout << "FOLLOWS\n";
-    for (auto x: Follows){
+    for (auto x: Follows) {
         std::cout << x.first << ":  ";
         for (auto v: x.second)
             std::cout << v << ' ';
--- CFGHandler/CFGHandler.h
+++ CFGHandler/CFGHandler.h
@@ -1,16 +1,6 @@
 #ifndef COMPILADORES_TEXTILES_CFGHANDLER_H
 #define COMPILADORES_TEXTILES_CFGHANDLER_H
 
-/*
- * std::string grammar, std::string terminals, std::string non_terminals, std::string starti
-   Ejemplo gramática: "A: 0 Number $ Number: 1 2 Number 2 1 | 0 $";
-
-   std::string grammar = "lexp: atom | list $ atom: number | identifier $ list: ( lexpseq ) $ lexpseq: lexp lexpseq | lexp $";
-   CFGHandler ff(grammar, "number identifier ( )", "lexp atom list lexpseq", "lexp");
-   auto pair = ff.get_firsts_follows();
-   ff.print();
- */
-
 #include <string>
 #include <vector>
 #include <unordered_set>
@@ -19,20 +9,20 @@
 #include <iostream>
 
 using identifier_t = std::string;
-struct Element_t{
+struct Element_t {
     identifier_t id;
     bool is_ter;
 };
 
-using Map_Int_String_t = std::unordered_map<int , std::string>;
+using Map_Int_String_t = std::unordered_map<int, std::string>;
 using Map_String_Int_t = std::unordered_map<std::string, int>;
 
 using Production_t = std::vector<identifier_t>;
 using Rule_t = std::vector<Production_t>;
 using SetS_t = std::unordered_set<identifier_t>;
 using SetInt_t = std::unordered_set<int>;
-using MapSets_t = std::unordered_map<identifier_t , SetS_t>;
-using Grammar_t = std::unordered_map<identifier_t , Rule_t>;
+using MapSets_t = std::unordered_map<identifier_t, SetS_t>;
+using Grammar_t = std::unordered_map<identifier_t, Rule_t>;
 
 class Parser;
 
@@ -47,28 +37,34 @@ class CFGHandler {
     std::string initial;
 
     void read_grammar(std::string grammari);
-    void read_elements(std::string elementsi, bool is_ter=true);
+
+    void read_elements(std::string elementsi, bool is_ter = true);
+
     void generate_firsts_follows();
 
 public:
-    CFGHandler (std::string grammar, std::string terminals, std::string non_terminals, std::string starti);
-    inline Grammar_t get_grammar(){
+    CFGHandler(std::string grammar, std::string terminals, std::string non_terminals, std::string starti);
+
+    inline Grammar_t get_grammar() {
         return grammar;
     };
-    inline SetS_t get_terminals(){
+
+    inline SetS_t get_terminals() {
         return terminals;
     };
-    inline SetS_t get_non_terminals(){
+
+    inline SetS_t get_non_terminals() {
         return non_terminals;
     };
+
     inline std::pair<MapSets_t, MapSets_t> get_firsts_follows() {
         return std::make_pair(Firsts, Follows);
     };
+
     void print();
 
     friend class Parser;
 };
-
 
 
 #endif //COMPILADORES_TEXTILES_CFGHANDLER_H
